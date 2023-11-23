1c0968228f6dbc6c45b713505c5f66add3530d8c
Stephen <stephendwalley@gmail.com>
2023-11-23 19:59:40 +1100

    Uploading vectors to supabase fully implemented

--- langchain-test/index.js
+++ langchain-test/index.js
@@ -2,23 +2,38 @@ import { CSVLoader } from "langchain/document_loaders/fs/csv";
 
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
+import { createClient } from "@supabase/supabase-js";
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+
+import 'dotenv/config'
+
 try {
     const loader = new CSVLoader("ndis-src-docs/NDIS-Catalogue.csv");
 
     const docs = await loader.load();
-    
+
     const splitter = new RecursiveCharacterTextSplitter();
-    
+
     const output = await splitter.splitDocuments(docs);
 
-    console.log(output);
+    //console.log(output);
 
     const sbApiKey = process.env.SUPABASE_API_KEY;
     const sbUrl = process.env.SUPABASE_URL;
     const openAIApiKey = process.env.OPENAI_API_KEY;
 
-    
-    
+    const client = createClient(sbUrl, sbApiKey);
+
+    await SupabaseVectorStore.fromDocuments(
+        output,
+        new OpenAIEmbeddings({ openAIApiKey }),
+        {
+            client,
+            tableName: "documents",
+        }
+    );
+
 } catch (e) {
     console.log(e);
 }
