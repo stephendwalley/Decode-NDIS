e225caa1e2a4c3b290eb1f2ea50d2730e91831fc
Stephen <stephendwalley@gmail.com>
2023-11-24 12:47:50 +1100

    Assistants retrieval implemented

--- assistants-api/app.js
+++ assistants-api/app.js
@@ -17,12 +17,40 @@ const file = await openai.files.create({
 
 
 
-const assistant = await openai.beta.assistants.create({
-    name: "Decode NDIS",
-    instructions: "You are an assistant that will accurate match item or activity descriptions for NDIS participants to the most applicable NDIS codes. Along with this you will provide the price caps, and other relevant information from the context including any outlying rules that may be important for the participant.",
-    tools: [{
-        type: "retrieval",
-    }],
-    model: "gpt-4-1106-preview",
-    file_ids: [file.id]
-})
+// const assistant = await openai.beta.assistants.create({
+//     name: "Decode NDIS",
+//     instructions: "You are an assistant that will accurate match item or activity descriptions for NDIS participants to the most applicable NDIS codes. Along with this you will provide the price caps, and other relevant information from the context including any outlying rules that may be important for the participant.",
+//     tools: [{
+//         type: "retrieval",
+//     }],
+//     model: "gpt-4-1106-preview",
+//     file_ids: [file.id]
+// })
+
+// Retrieve created assistant
+const assistant = await openai.beta.assistants.retrieve("asst_zVybTcQQK8WjIDNCEVi3ao5u")
+
+// Thread creation
+const thread = await openai.beta.threads.create();
+
+
+// Create message
+const message = await openai.beta.threads.messages.create(thread.id, {
+    role: "user",
+    content: "What code should I apply podiatry to?"
+});
+
+// Running assistant
+const run = await openai.beta.threads.runs.create(thread.id, {
+    assistant_id: assistant.id,
+});
+
+console.log(run)
+
+// const messages = await openai.beta.threads.messages.list(thread.id);
+
+// messages.body.data.forEach((message) => {
+//     console.log(message.content)
+// })
+
+const 
